8a8f4dbf7c43c4f44f95d0de0565475c40bebc8f
Kilo <yomumashusband@gmail.com>
2018-12-24 22:03:56 +0000

    f
    f

---
+++ uc2/uc2.js
@@ -0,0 +1,7 @@
+var sp = document.getElementById('span');
+var inp1 = document.getElementById('inp');
+var btn1 = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+  sp.innerHTML = '&#'+inp.value;
+})
